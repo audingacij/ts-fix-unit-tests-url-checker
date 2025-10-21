d01e5cd0f708bad47ef1e10bdfbc9268c0d0d178
Audinga.Cijunaityte <audingacij@gmail.com>
2025-10-21 15:27:46 +0300

    Added more tests and updated the pipeline to run password-strength.test.ts

--- tests/password-strength.test.ts
+++ tests/password-strength.test.ts
@@ -5,17 +5,35 @@ describe('Calculate password strength', () => {
         expect(calculatePasswordStrength('abc')).toBe('Very Weak');
     });
 
+    test('Very weak for only lowercase letters', () => {
+        expect(calculatePasswordStrength('abcdefgh')).toBe('Very Weak');
+    });
+
     test('Weak for password with numbers but short', () => {
         expect(calculatePasswordStrength('Abc123')).toBe('Weak');
     });
 
-    test('Moderate for longer password with letters', () => {
+    test('Weak for password with numbers but lowercase', () => {
+        expect(calculatePasswordStrength('abc12356')).toBe('Weak');
+    });
+
+    test('Moderate for longer password with letters, numbers and uppercase', () => {
         expect(calculatePasswordStrength('abcdefGh1')).toBe('Moderate');
+    });
+
+    test('Moderate for 13 characters but lowercase', () => {
+        expect(calculatePasswordStrength('abcdef1234567')).toBe('Moderate');
     });
 
     test('Strong for complex password', () => {
         expect(calculatePasswordStrength('Abcdef1!23')).toBe('Strong');
     });
 
+    test('Strong for 13 characters with all types', () => {
+        expect(calculatePasswordStrength('Abcd12345!@#$')).toBe('Strong');
+    });
 
+    test('Strong for 10 characters with all types', () => {
+        expect(calculatePasswordStrength('Abcdef1!23')).toBe('Strong');
+    });
 });
